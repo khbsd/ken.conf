695a2e8a5fce1edb313eaa21af4188a7eccbf37b
khb <khb.dev@gmail.com>
2021-11-03 11:45:27 -0500

    add FreeBSD-based rc script

---
+++ .loginrc/beastie-start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#sxhkd -c /usr/home/ken/.config/sxhkd/sxhkdrc &
+xdotool key Num_Lock &
+feh --bg-fill /usr/home/ken/Pictures/Wallpapers/peeter-maimik-solarpunk-painting-peeter-maimik.jpg &
+spotifyd &
+playerctl & 
+picom --experimental-backends &
