f5f32dbccdb14825f02a280fa10392f1eba87a89
khb <khb.dev@gmail.com>
2021-12-02 18:20:58 -0600

    update new branch for BSD-specific confs

--- .loginrc/beastie-start.sh
+++ .loginrc/beastie-start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-#sxhkd -c /usr/home/ken/.config/sxhkd/sxhkdrc &
+sxhkd -c /usr/home/ken/.config/sxhkd/sxhkdrc &
 xdotool key Num_Lock &
-feh --bg-fill /usr/home/ken/Pictures/Wallpapers/peeter-maimik-solarpunk-painting-peeter-maimik.jpg &
+feh --bg-scale /home/ken/Pictures/Wallpapers/gruvbox_jocelin-carmes-giant-skeleton-barn.jpg
+xsettingsd &
 spotifyd &
 playerctl & 
 picom --experimental-backends &
